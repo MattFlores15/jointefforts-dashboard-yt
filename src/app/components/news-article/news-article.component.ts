d84d8f10b46c32e9a638d8ae3148a19a3d3b7cf2
Matias Flores <matias.flores@logichainge.com>
2023-12-29 20:57:03 +0100

    Design improvements - all

--- src/app/components/news-article/news-article.component.ts
+++ src/app/components/news-article/news-article.component.ts
@@ -1,12 +1,52 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-news-article',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './news-article.component.html',
   styleUrl: './news-article.component.css'
 })
 export class NewsArticleComponent {
 
+   // Add properties to manage reply visibility
+  showReplyComment1: boolean = false;
+  showReplyComment2: boolean = false;
+  // Function to toggle the visibility of replies for Comment 1
+  toggleReply(comment: string): void {
+    if (comment === 'comment1') {
+      this.showReplyComment1 = !this.showReplyComment1;
+      // Close the reply for Comment 2 when opening Comment 1's reply
+      this.showReplyComment2 = false;
+    } else if (comment === 'comment2') {
+      this.showReplyComment2 = !this.showReplyComment2;
+      // Close the reply for Comment 1 when opening Comment 2's reply
+      this.showReplyComment1 = false;
+    }
+  }
+
+  // Function to check if the reply for Comment 1 should be shown
+  showReply(comment: string): boolean {
+    if (comment === 'comment1') {
+      return this.showReplyComment1;
+    } else if (comment === 'comment2') {
+      return this.showReplyComment2;
+    }
+    return false;
+  }
+  // Variable to store user feedback (initially set to null)
+  feedback: 'thumbsUp' | 'thumbsDown' | null = null;
+
+  // Method to handle user feedback
+  handleFeedback(selectedFeedback: 'thumbsUp' | 'thumbsDown'): void {
+    if (this.feedback === selectedFeedback) {
+      // If the same option is clicked again, unselect it
+      this.feedback = null;
+    } else {
+      // Otherwise, update the selected feedback
+      this.feedback = selectedFeedback;
+    }
+  }
 }
--- src/app/components/news/news.component.ts
+++ src/app/components/news/news.component.ts
@@ -17,14 +17,14 @@ export class NewsComponent {
 
   articles = [
     // Articles 4-7
-    { image: "../../assets/images/pexels-juan-c-palacios-4069882.jpg" , title: "Nurturing Tomorrow's Healthcare Heroes: Survey Program's Focus on Training", summary: "Delve into the ways the survey program is contributing to the development of healthcare professionals..." },
+    { image: "../../assets/images/pexels-juan-c-palacios-4069882.jpg" , title: "Nurturing Tomorrow's Healthcare Heroes: Survey Program's Focus on Training", summary: "Delve into the ways the survey program is contributing to the development of healthcare professionals...",bookmarked: false },
+    { image: "../../assets/images/pexels-antoni-shkraba-5214994.jpg", title: "Tech Solutions for Staffing Woes: Survey Program Spurs Digital Innovations", summary: "Discover how the survey program's insights are catalyzing technological advancements in healthcare staffing...", bookmarked: false },
+    { image: "../../assets/images/pexels-karolina-grabowska-8538867.jpg", title: "Community Engagement: A Surprising Solution to Healthcare Staff Shortages", summary: "Uncover the community-focused strategies emerging from the survey program...", bookmarked: false },
+    { image: "../../assets/images/pexels-rdne-stock-project-6129507.jpg", title: "Survey Program Success Stories: Real Impact on Healthcare Professionals", summary: "Hear firsthand accounts from healthcare workers whose lives have been positively influenced by the survey program...", bookmarked: false },
+    { image: "../../assets/images/pexels-juan-c-palacios-4069882.jpg" , title: "Nurturing Tomorrow's Healthcare Heroes: Survey Program's Focus on Training", summary: "Delve into the ways the survey program is contributing to the development of healthcare professionals...",bookmarked: false },
     { image: "../../assets/images/pexels-antoni-shkraba-5214994.jpg", title: "Tech Solutions for Staffing Woes: Survey Program Spurs Digital Innovations", summary: "Discover how the survey program's insights are catalyzing technological advancements in healthcare staffing..." },
-    { image: "../../assets/images/pexels-karolina-grabowska-8538867.jpg", title: "Community Engagement: A Surprising Solution to Healthcare Staff Shortages", summary: "Uncover the community-focused strategies emerging from the survey program..." },
-    { image: "../../assets/images/pexels-rdne-stock-project-6129507.jpg", title: "Survey Program Success Stories: Real Impact on Healthcare Professionals", summary: "Hear firsthand accounts from healthcare workers whose lives have been positively influenced by the survey program..." },
-    { image: "../../assets/images/pexels-juan-c-palacios-4069882.jpg" , title: "Nurturing Tomorrow's Healthcare Heroes: Survey Program's Focus on Training", summary: "Delve into the ways the survey program is contributing to the development of healthcare professionals..." },
-    { image: "../../assets/images/pexels-antoni-shkraba-5214994.jpg", title: "Tech Solutions for Staffing Woes: Survey Program Spurs Digital Innovations", summary: "Discover how the survey program's insights are catalyzing technological advancements in healthcare staffing..." },
-    { image: "../../assets/images/pexels-karolina-grabowska-8538867.jpg", title: "Community Engagement: A Surprising Solution to Healthcare Staff Shortages", summary: "Uncover the community-focused strategies emerging from the survey program..." },
-    { image: "../../assets/images/pexels-rdne-stock-project-6129507.jpg", title: "Survey Program Success Stories: Real Impact on Healthcare Professionals", summary: "Hear firsthand accounts from healthcare workers whose lives have been positively influenced by the survey program..." },
+    { image: "../../assets/images/pexels-karolina-grabowska-8538867.jpg", title: "Community Engagement: A Surprising Solution to Healthcare Staff Shortages", summary: "Uncover the community-focused strategies emerging from the survey program...",bookmarked: false },
+    { image: "../../assets/images/pexels-rdne-stock-project-6129507.jpg", title: "Survey Program Success Stories: Real Impact on Healthcare Professionals", summary: "Hear firsthand accounts from healthcare workers whose lives have been positively influenced by the survey program...",bookmarked: false },
   ];
 
  // Custom slider logic
@@ -60,5 +60,11 @@ export class NewsComponent {
     }
   }
 
+  // i: number = 0;
+
+  // Inside your component class
+toggleBookmark(article: any): void {
+  article.bookmarked = !article.bookmarked;
+}
 
 }
