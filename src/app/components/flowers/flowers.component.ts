599fb37374fb480ec42dc613dee6c44311eb831e
Matias Flores <matias.flores@logichainge.com>
2024-01-13 17:28:23 +0100

    Reward page - flowers images + badges + pot name

--- src/app/components/flowers/flowers.component.ts
+++ src/app/components/flowers/flowers.component.ts
@@ -18,10 +18,10 @@ interface Flower {
 @Component({
   selector: 'app-flowers',
   standalone: true,
-  imports: [CommonModule,],
+  imports: [CommonModule],
   templateUrl: './flowers.component.html',
   styleUrls: ['./flowers.component.css'],
-   animations: [
+  animations: [
     trigger('popUpAnimation', [
       state('*', style({ transform: 'scale(1)', opacity: 1 })),
       transition(':enter', [
@@ -36,10 +36,144 @@ interface Flower {
 })
 export class FlowersComponent {
 
-   flowers: Flower[] = [
-    { id: 'sunflower', name: 'Sunflower', image: '../../../assets/images/plants/sunflower-planted.png', description: 'Sunflowers are known for being “happy” flowers, making them the perfect gift to bring joy to someone\'s (or your) day.' },
-    { id: 'rose', name: 'Rose', image: '../../../assets/images/plants/rose-planted.png', description: 'Roses are a classic flower that symbolize love, romance, and friendship. They come in many different colors and each color has a different meaning.' },
-    { id: 'lavender', name: 'Lavender', image: '../../../assets/images/plants/lavender-planted.png', description: 'Lavender is known for its strong, calming scent. It is often used in aromatherapy to promote relaxation and reduce stress.' },
+  flowers: Flower[] = [
+    {
+      id: 'sunflower',
+      name: 'Sunflower',
+      image: '../../../assets/images/plants/sunflower-planted.png',
+      description: `
+        Deze plant heeft geholpen aan de 
+        <a href="/components/results/results.html"><b><u>maandelijkse vragenlijst.</b></u></a>
+        De resultaten van het onderzoek worden nog besproken en het gevolg hiervan kun je later bij de plant zien.
+        <h2>Wist je dat?</h2>
+        De bloemknoppen en jonge bloemen draaien langzaam mee met de zon als deze zich
+        verplaatst van oost naar west. De zonnebloem bloeit op grote hoogte. Sommige
+        soorten kunnen wel 3 meter hoog worden. De meesten groeien echter tot 1,5
+        tot 2,5 meter.
+      `,
+    },
+    {
+      id: 'rose',
+      name: 'Rose',
+      image: '../../../assets/images/plants/rose-planted.png',
+      description: `
+        Deze plant heeft geholpen aan de 
+        <a href="/components/results/results.html"><b><u>wekelijkse vragenlijst.</b></u></a>
+        De resultaten van het onderzoek worden nog besproken en het gevolg hiervan kun je later bij de plant zien.
+        <h2>Wist je dat?</h2>
+        Wist je dat rozen in Nederland bekend zijn vanaf de zestiende
+        eeuw. Toen werden rozen vooral geteeld in formele paleistuinen. In
+        1829 bestonden er al 2562 verschillende soorten rozen. Maar
+        mogelijk is de teelt van rozen al zo'n 5000 jaar geleden in de
+        buurt van de Indus-vallei of in China voor het eerst ontstaan.
+      `,
+    },
+    {
+      id: 'lavender',
+      name: 'Lavender',
+      image: '../../../assets/images/plants/lavender-planted.png',
+      description: `
+        Deze plant heeft geholpen aan de 
+        <a href="/components/news-articles/results-article.html"><b><u>halfjaarlijkse vragenlijst.</b></u></a>
+        De resultaten van het onderzoek zijn dat de vragenlijsten korter en leuker moeten, daarom ook deze applicatie.
+        <h2>Wist je dat?</h2>
+        Lavendel wordt door veel mensen gezien als medicinaal middel.
+        Het zou kalmerend zijn voor de zenuwen en het zou de geest in balans brengen.
+        Tip: leg een takje lavendel naast je kussen in bed. Geeft een rustig gevoel én een fijn geurtje!
+        Je treft het zo nu en dan ook in Franse hotels aan.
+      `,
+    },
+    {
+      id: 'daisy',
+      name: 'Daisy',
+      image: '../../../assets/images/plants/daisy-planted.png',
+      description: `
+        Deze plant heeft geholpen aan de 
+        <a href="/components/results/results.html"><b><u>wekelijkse vragenlijst.</b></u></a>
+        De resultaten van het onderzoek worden nog besproken en het gevolg hiervan kun je later bij de plant zien.
+        <h2>Wist je dat?</h2>
+        Madeliefjes symboliseren onschuld en puurheid. Ze worden vaak geassocieerd met
+        jeugdige liefde en romantiek.
+      `,
+    },
+    {
+      id: 'tulip',
+      name: 'Tulip',
+      image: '../../../assets/images/plants/tulip-planted.png',
+      description: `
+        Deze plant heeft geholpen aan de 
+        <a href="/components/news-articles/results-article.html"><b><u>kwartaallijkse vragenlijst.</b></u></a>
+        De resultaten van het onderzoek zijn dat de vragenlijsten korter en leuker moeten, daarom ook deze applicatie.
+        <h2>Wist je dat?</h2>
+        Tulpen zijn oorspronkelijk afkomstig uit Turkije en werden in de 16e eeuw naar Nederland gebracht.
+        In de 17e eeuw was er zelfs een economische bubbel bekend als "Tulpenmanie" vanwege de hoge prijzen van tulpenbollen.
+      `,
+    },
+    {
+      id: 'orchid',
+      name: 'Orchid',
+      image: '../../../assets/images/plants/orchid-planted.png',
+      description: `
+        Deze plant heeft geholpen aan de 
+        <a href="/components/news-articles/results-article.html"><b><u>kwartaallijkse vragenlijst.</b></u></a>
+        De resultaten van het onderzoek zijn dat de vragenlijsten korter en leuker moeten, daarom ook deze applicatie.
+        <h2>Wist je dat?</h2>
+        Orchideeën zijn een van de grootste plantenfamilies ter wereld, met meer dan 25.000 soorten.
+        Ze staan ​bekend om hun diverse bloemvormen en kleuren.
+      `,
+    },
+    {
+      id: 'lily',
+      name: 'Lily',
+      image: '../../../assets/images/plants/lily-planted.png',
+      description: `
+        Deze plant heeft geholpen aan de 
+        <a href="/components/results/results.html"><b><u>maandelijkse vragenlijst.</b></u></a>
+        De resultaten van het onderzoek worden nog besproken en het gevolg hiervan kun je later bij de plant zien.
+        <h2>Wist je dat?</h2>
+        Lelies symboliseren zuiverheid en majesteit. Ze worden vaak geassocieerd met
+        goddelijke betekenis in verschillende culturen.
+      `,
+    },
+    {
+      id: 'carnation',
+      name: 'Carnation',
+      image: '../../../assets/images/plants/carnation-planted.png',
+      description: `
+        Deze plant heeft geholpen aan de 
+        <a href="/components/results/results.html"><b><u>wekelijkse vragenlijst.</b></u></a>
+        De resultaten van het onderzoek worden nog besproken en het gevolg hiervan kun je later bij de plant zien.
+        <h2>Wist je dat?</h2>
+        Anjers worden vaak geassocieerd met liefde en genegenheid. In verschillende kleuren
+        hebben ze verschillende symbolische betekenissen.
+      `,
+    },
+    {
+      id: 'daffodil',
+      name: 'Daffodil',
+      image: '../../../assets/images/plants/daffodil-planted.png',
+      description: `
+        Deze plant heeft geholpen aan de 
+        <a href="/components/news-articles/results-article.html"><b><u>halfjaarlijkse vragenlijst.</b></u></a>
+        De resultaten van het onderzoek zijn dat de vragenlijsten korter en leuker moeten, daarom ook deze applicatie.
+        <h2>Wist je dat?</h2>
+        Narcissen worden vaak geassocieerd met lenteseizoen en symboliseren
+        vernieuwing en wedergeboorte.
+      `,
+    },
+    {
+      id: 'cactus',
+      name: 'Cactus',
+      image: '../../../assets/images/plants/cactus-planted.png',
+      description: `
+        Deze plant heeft geholpen aan de 
+        <a href="/components/news-articles/results-article.html"><b><u>kwartaallijkse vragenlijst.</b></u></a>
+        De resultaten van het onderzoek zijn dat de vragenlijsten korter en leuker moeten, daarom ook deze applicatie.
+        <h2>Wist je dat?</h2>
+        Cactussen staan bekend om hun vermogen om te overleven in droge omgevingen.
+        Ze slaan water op in hun stengels en kunnen lange perioden zonder regen overleven.
+      `,
+    },
   ];
 
   visibleFlowerId: string | null = null;
