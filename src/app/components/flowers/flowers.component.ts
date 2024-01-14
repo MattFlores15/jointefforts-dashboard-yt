af32d3a73291ee762e17acb81f903d9c29e93ca9
Matias Flores <matias.flores@logichainge.com>
2024-01-14 03:46:07 +0100

    Flowers - blank flower setup + layout fix & hover fix

--- src/app/components/flowers/flowers.component.ts
+++ src/app/components/flowers/flowers.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {
   trigger,
   state,
@@ -17,6 +17,8 @@ interface Flower {
   helping?: string;
   question?: string;
   badge?: string;
+  unlocked?: boolean;
+  initiallyUnlocked?: boolean;
 }
 
 @Component({
@@ -38,30 +40,124 @@ interface Flower {
     ]),
   ],
 })
-export class FlowersComponent {
+export class FlowersComponent implements OnInit{
+
+ // Function to determine initially unlocked flowers
+ngOnInit(): void {
+    this.initializeFlowers();
+    this.randomlyUnlockFlowers();
+  }
+
+  initializeFlowers(): void {
+    const initiallyUnlockedCount = 3;
+    const initiallyUnlockedFlowers: Flower[] = this.flowers
+      .filter((flower) => flower.initiallyUnlocked)
+      .slice(0, initiallyUnlockedCount);
+
+    const remainingSlotsCount = 10 - initiallyUnlockedFlowers.length;
+    const noFlowerSlots: Flower[] = Array.from({ length: remainingSlotsCount }, (_, index) => ({
+      id: `no-flower-${index + 1}`,
+      name: 'No flower',
+      image: '../../../assets/images/plants/no-planted.png',
+      badge: '../../../assets/images/plants/badges/no-badge.png',
+      description: `<p>Je hebt nog geen bloem geplant.</p>`,
+      unlocked: false,
+      initiallyUnlocked: false,
+    }));
+
+    const unlockableFlowers = this.flowers.filter(
+      (flower) =>
+        flower.id !== 'no-flower' &&
+        flower.id !== 'sunflower' &&
+        flower.id !== 'rose' &&
+        flower.id !== 'lavender' &&
+        !flower.unlocked &&
+        !flower.initiallyUnlocked
+    );
+
+    const randomlyUnlockedElements = this.shuffleArray(unlockableFlowers).slice(0, 2);
+
+    randomlyUnlockedElements.forEach((flower) => {
+      flower.unlocked = true;
+    });
+
+    this.flowers = initiallyUnlockedFlowers.concat(noFlowerSlots);
+  }
+
+
+  // Function to randomly unlock 2 "Flower" elements
+ randomlyUnlockFlowers(): void {
+  const unlockableFlowers = this.flowers.filter(
+    (flower) =>
+      !flower.id.startsWith('no-flower') &&
+      flower.id !== 'sunflower' &&
+      flower.id !== 'rose' &&
+      flower.id !== 'lavender' &&
+      !flower.unlocked
+  );
+
+  console.log('Unlockable Flowers:', unlockableFlowers);
+
+  const randomlyUnlockedElements = this.shuffleArray(unlockableFlowers).slice(0, 2);
+
+  console.log('Randomly Unlocked Flowers:', randomlyUnlockedElements);
+
+  randomlyUnlockedElements.forEach((flower) => {
+    flower.unlocked = true;
+  });
+
+  console.log('Updated Flowers Array:', this.flowers);
+}
+
+  // Shuffle array function
+  shuffleArray<T>(array: T[]): T[] {
+  let currentIndex = array.length;
+  let randomIndex: number;
+
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+  }
+
+  console.log(array); // Log the shuffled array
+
+  return array;
+}
+
 
 // Add a new property to track whether the mouse is over the popup
 mouseOverPopup: boolean = false;
 
   flowers: Flower[] = [
     {
+      id:'no-flower',
+      name: 'No flower',
+      image: '../../../assets/images/plants/no-planted.png',
+      badge:'../../../assets/images/plants/badges/no-badge.png',
+      description: `
+        <p>Je hebt nog geen bloem geplant.</p>
+      `,
+      unlocked: false,
+      initiallyUnlocked: false,
+    },
+    {
       id: 'sunflower',
       name: 'Sunflower',
       image: '../../../assets/images/plants/sunflower-planted.png',
       badge:'../../../assets/images/plants/badges/sunflower.png',
-      helping: `
-        <p>Deze plant heeft geholpen aan de <a routerLink="/results">maandelijkse vragenlijst.</a>
-        De resultaten van het onderzoek worden nog besproken en het gevolg hiervan kun je later bij de plant zien.</p>`,
-      question: '<h2>Wist je dat?</h2>',
-      description: '<p>De bloemknoppen en jonge bloemen draaien langzaam mee met de zon als <p>deze zich verplaatst van oost naar west. De zonnebloem bloeit op grote hoogte. Sommige soorten kunnen wel 3 meter hoog worden. De meesten groeien echter tot 1,5 tot 2,5 meter.</p>' ,
+      question: '<i class="fa-solid fa-book"></i><h2>Wist je dat?</h2>',
+      description: '<p>De bloemknoppen en jonge bloemen draaien langzaam mee met de zon als <p>deze zich verplaatst van oost naar west. De zonnebloem bloeit op grote hoogte. Sommige soorten kunnen wel 3 meter hoog worden.</p>' ,
+      unlocked: true,
+      initiallyUnlocked: true,
     },
     {
       id: 'rose',
       name: 'Rose',
       image: '../../../assets/images/plants/rose-planted.png',
       badge:'../../../assets/images/plants/badges/rose.png',
-      helping: '<p>Deze plant heeft geholpen aan de <a routerLink="/results">wekelijkse vragenlijst.</a> De resultaten van het onderzoek worden nog besproken en het gevolg hiervan kun je later bij de plant zien.</p>',
-      question: '<h2>Wist je dat?</h2>',
+      question: '<i class="fa-solid fa-book"></i><h2>Wist je dat?</h2>',
       description: `
         <p>Wist je dat rozen in Nederland bekend zijn vanaf de zestiende
         eeuw. Toen werden rozen vooral geteeld in formele paleistuinen. In
@@ -69,98 +165,95 @@ mouseOverPopup: boolean = false;
         mogelijk is de teelt van rozen al zo'n 5000 jaar geleden in de
         buurt van de Indus-vallei of in China voor het eerst ontstaan.</p>
       `,
+      unlocked: true,
+      initiallyUnlocked: true,
     },
     {
       id: 'lavender',
       name: 'Lavender',
       image: '../../../assets/images/plants/lavender-planted.png',
       badge:'../../../assets/images/plants/badges/lavender.png',
-      helping: `
-        <p>Deze plant heeft geholpen aan de <a routerLink="/results">halfjaarlijkse vragenlijst.</a>
-        De resultaten van het onderzoek zijn dat de vragenlijsten korter en leuker moeten, daarom ook <p>deze applicatie.</p>`,
-      question: '<h2>Wist je dat?</h2>',
+      question: '<i class="fa-solid fa-book"></i><h2>Wist je dat?</h2>',
       description: `
         <p>Lavendel wordt door veel mensen gezien als medicinaal middel.
         Het zou kalmerend zijn voor de zenuwen en het zou de geest in balans brengen.
         Tip: leg een takje lavendel naast je kussen in bed. Geeft een rustig gevoel én een fijn geurtje!
         Je treft het zo nu en dan ook in Franse hotels aan.</p>
       `,
+      unlocked: true,
+      initiallyUnlocked: true,
     },
     {
       id: 'daisy',
       name: 'Daisy',
       image: '../../../assets/images/plants/daisy-planted.png',
       badge:'../../../assets/images/plants/badges/daisy.png',
-      helping: `
-        <p>Deze plant heeft geholpen aan de <a routerLink="/results">wekelijkse vragenlijst.</a>
-        De resultaten van het onderzoek worden nog besproken en het gevolg hiervan kun je later bij de plant zien.</p>`,
-      question: '<h2>Wist je dat?</h2>',
+      question: '<i class="fa-solid fa-book"></i><h2>Wist je dat?</h2>',
       description: `
         <p>Madeliefjes symboliseren onschuld en puurheid. Ze worden vaak geassocieerd met
         jeugdige liefde en romantiek.</p>
       `,
+      unlocked: false,
+      initiallyUnlocked: false,
     },
     {
       id: 'tulip',
       name: 'Tulip',
       image: '../../../assets/images/plants/tulip-planted.png',
       badge:'../../../assets/images/plants/badges/tulip.png',
-      helping: `
-        <p>Deze plant heeft geholpen aan de <a routerLink="/results">kwartaallijkse vragenlijst.</a>
-        De resultaten van het onderzoek zijn dat de vragenlijsten korter en leuker moeten, daarom ook <p>deze applicatie.</p>`,
-      question: '<h2>Wist je dat?</h2>',
+      question: '<i class="fa-solid fa-book"></i><h2>Wist je dat?</h2>',
       description: `
         <p>Tulpen zijn oorspronkelijk afkomstig uit Turkije en werden in de 16e eeuw naar Nederland gebracht.
         In de 17e eeuw was er zelfs een economische bubbel bekend als "Tulpenmanie" vanwege de hoge prijzen van tulpenbollen.</p>
       `,
+      unlocked: false,
+      initiallyUnlocked: false,
     },
     {
       id: 'orchid',
       name: 'Orchid',
       image: '../../../assets/images/plants/orchid-planted.png',
       badge:'../../../assets/images/plants/badges/orchid.png',
-      helping: '<p>Deze plant heeft geholpen aan de <a routerLink="/results">halfjaarlijkse vragenlijst.</a> De resultaten van het onderzoek zijn dat de vragenlijsten korter en leuker moeten, daarom ook <p>deze applicatie.</p>',
-      question: '<h2>Wist je dat?</h2>',
+      question: '<i class="fa-solid fa-book"></i><h2>Wist je dat?</h2>',
       description: `
         <p>Orchideeën zijn een van de grootste plantenfamilies ter wereld, met meer dan 25.000 soorten.
         Ze staan ​bekend om hun diverse bloemvormen en kleuren.</p>
       `,
+      unlocked: false,
+      initiallyUnlocked: false,
     },
     {
       id: 'lily',
       name: 'Lily',
       image: '../../../assets/images/plants/lily-planted.png',
       badge:'../../../assets/images/plants/badges/lily.png',
-      helping: ' <p>Deze plant heeft geholpen aan de <a routerLink="/results">maandelijkse vragenlijst.</a> De resultaten van het onderzoek worden nog besproken en het gevolg hiervan kun je later bij de plant zien.</p>',
-      question: '<h2>Wist je dat?</h2>',
+      question: '<i class="fa-solid fa-book"></i><h2>Wist je dat?</h2>',
       description: `
         <p>Lelies symboliseren zuiverheid en majesteit. Ze worden vaak geassocieerd met
         goddelijke betekenis in verschillende culturen.</p>
       `,
+      unlocked: false,
+      initiallyUnlocked: false,
     },
     {
       id: 'carnation',
       name: 'Carnation',
       image: '../../../assets/images/plants/carnation-planted.png',
       badge:'../../../assets/images/plants/badges/carnation.png',
-      helping: `
-        <p>Deze plant heeft geholpen aan de <a routerLink="/results">wekelijkse vragenlijst.</a>
-        De resultaten van het onderzoek worden nog besproken en het gevolg hiervan kun je later bij de plant zien.</p>`,
-      question: '<h2>Wist je dat?</h2>',
+      question: '<i class="fa-solid fa-book"></i><h2>Wist je dat?</h2>',
       description: `
         <p>Anjers worden vaak geassocieerd met liefde en genegenheid. In verschillende kleuren
         hebben ze verschillende symbolische betekenissen.</p>
       `,
+      unlocked: false,
+      initiallyUnlocked: false,
     },
     {
       id: 'daffodil',
       name: 'Daffodil',
       image: '../../../assets/images/plants/daffodil-planted.png',
       badge:'../../../assets/images/plants/badges/daffodil.png',
-      helping: `
-        <p>Deze plant heeft geholpen aan de <a routerLink="/results">kwartaallijkse vragenlijst.</a>
-        De resultaten van het onderzoek zijn dat de vragenlijsten korter en leuker moeten, daarom ook <p>deze applicatie.</p>`,
-      question: '<h2>Wist je dat?</h2>',
+      question: '<i class="fa-solid fa-book"></i><h2>Wist je dat?</h2>',
       description: `
         <p>Narcissen worden vaak geassocieerd met lenteseizoen en symboliseren
         vernieuwing en wedergeboorte.</p>
@@ -171,14 +264,13 @@ mouseOverPopup: boolean = false;
       name: 'Cactus',
       image: '../../../assets/images/plants/cactus-planted.png',
       badge:'../../../assets/images/plants/badges/cactus.png',
-      helping: `
-        <p>Deze plant heeft geholpen aan de <a routerLink="/results">halfjaarlijkse vragenlijst.</a>
-        De resultaten van het onderzoek zijn dat de vragenlijsten korter en leuker moeten, daarom ook <p>deze applicatie.</p>`,
-      question: '<h2>Wist je dat?</h2>',
+      question: '<i class="fa-solid fa-book"></i><h2>Wist je dat?</h2>',
       description: `
         <p>Cactussen staan bekend om hun vermogen om te overleven in droge omgevingen.
         Ze slaan water op in hun stengels en kunnen lange perioden zonder regen overleven.</p>
       `,
+      unlocked: false,
+      
     },
   ];
 
